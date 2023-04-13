0567ef07df630b8b71f610a657e20bc68bddc2b8
Bennyskiee <202080019@psu.palawan.edu.ph>
2023-04-13 22:17:33 +0800

    done copy

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+	define('ROOT_URL', 'http://localhost/task');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root@localhost');
+	define('DB_PASS', '');
+	define('DB_NAME', 'task');
+
+	?>
